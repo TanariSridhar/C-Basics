453b2e35cf2eeac8642bec135278652e15645d4a
TanariSridhar <92074927+TanariSridhar@users.noreply.github.com>
2021-10-22 10:54:56 +0530

    Add files via upload

---
+++ data types.c
@@ -0,0 +1,15 @@
+\*print of Data types\*
+#include<stdio.h>
+void main()
+{
+	// variable Declaration
+	int age;
+	float cgpa;
+	char intial;
+	
+	//initialization
+	age=19;
+	cgpa=7.0;
+	initial='T';
+}
+
---
+++ program using slash.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+void main()
+{
+	printf("my favourite place is \"Araku valley\"\n it is a Hill station");
+}
+	
