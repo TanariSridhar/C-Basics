5f807858bcce762cdc202b249931950ebe975c9c
TanariSridhar <92074927+TanariSridhar@users.noreply.github.com>
2021-10-23 10:57:36 +0530

    Add files via upload

---
+++ adding point values.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+// variable declaration
+float number1,number2,sum;
+
+//initialization
+  number1=28.56;
+  number2=33.50;
+
+//calculation
+sum=number1+number2; //28.56+33.50=62.06
+
+//output
+printf(" sum of %f and %f is %f",number1,number2,sum);
+}
--- data types.c
+++ data types.c
@@ -1,15 +1,18 @@
-\*print of Data types\*
+// Data type variable program
 #include<stdio.h>
 void main()
 {
 	// variable Declaration
 	int age;
 	float cgpa;
-	char intial;
-	
+	char initial;
 	//initialization
 	age=19;
 	cgpa=7.0;
 	initial='T';
+	// output
+	printf("Age of the student is %d\n",age);
+	printf("CGPA of the student is %f\n",cgpa);
+	printf("student surname starts with %c\n",initial);
 }
 
